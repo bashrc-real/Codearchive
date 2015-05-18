d3071e26ff802bdb08784df0d07a83cdc3f0a381
bashrc <saurabh8c@gmail.com>
2015-05-18 23:36:20 +0530

    Added an assert   Changes to be committed: 	modified:   dfs.h

--- adapter_algorithm/dfs.h
+++ adapter_algorithm/dfs.h
@@ -3,12 +3,20 @@
 #include <functional>
 #include <assert.h>
 
+#ifdef DBG
+#define ASSERT_DBG(x) assert(x)
+#else
+#define ASSERT_DBG(x)
+#endif
+
 template <typename T, typename func, typename container_type>
 void dfs(const T &node, func terminate, container_type getNext){
     class _impl {
         public:
             _impl(func _e, container_type _n) : m_terminate(std::move(_e)), m_getNext(std::move(_n)) {}
             void dfs(const T &node){
+                ASSERT_DBG(m_terminate);
+                ASSERT_DBG(m_getNext);
                 if (m_terminate(node)){
                     return;
                 }
