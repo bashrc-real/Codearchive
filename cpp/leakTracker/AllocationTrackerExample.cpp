04481eccd39b03f9420ef0557d96abab273bf7da
Saurabh Singh <sausingh@microsoft.com>
2015-08-15 15:10:26 +0530

    Added include guard to be compatible with all compilers. Added thread safety

--- cpp/leakTracker/AllocationTrackerExample.cpp
+++ cpp/leakTracker/AllocationTrackerExample.cpp
@@ -1,6 +1,11 @@
 #include "allocation_tracker.h"
 #include <memory>
 
+// Begin static member definition 
+std::unique_ptr<AllocMapper> AllocMapper::m_allocObject;
+std::mutex AllocMapper::m_lock;
+// end static member definition
+
 // compiled with TRACK_MEMORY macro defined with the build params
 // for eg. in g++ g++ -std=c++11 --pedantic -DTRACK_MEMORY <fileName>
 class foo{
