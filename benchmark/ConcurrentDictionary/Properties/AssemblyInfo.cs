bbc9d59e42956e39c033e1c098b9cd9cfc7c5e22
unknown <sausingh@bashrc440.fareast.corp.microsoft.com>
2016-03-07 13:46:30 +0530

    Fixing the assembly name

--- benchmark/ConcurrentDictionary/Properties/AssemblyInfo.cs
+++ benchmark/ConcurrentDictionary/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TryConnectDb")]
+[assembly: AssemblyTitle("ConcurrentDictionaryBenchmarking")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TryConnectDb")]
+[assembly: AssemblyProduct("ConcurrentDictionaryBenchmarking")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
